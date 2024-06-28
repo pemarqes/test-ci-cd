246b3ba47f307147f25838038873311335639144
Pedro Marques <pedmarques18@gmail.com>
2024-06-28 01:49:27 -0300

    test

--- .process/generate_comment.js
+++ .process/generate_comment.js
@@ -13,27 +13,49 @@ if (fs.existsSync(filePath)) {
 
     // Iterar sobre cada entrada no JSON
     data.forEach(entry => {
+        const engine = entry.engine;
+        const fileName = entry.fileName;
+
+        // Encontrar o índice onde 'force-app' termina no caminho do fileName
+        const forceAppIndex = fileName.indexOf('force-app');
+        if (forceAppIndex !== -1) {
+            // Obter o caminho após 'force-app'
+            const pathAfterForceApp = fileName.substring(forceAppIndex);
+
+            // Adicionar cabeçalho com engine e caminho após 'force-app'
+            markdownContent += `### Análise Estática de Código - ${engine}\n`;
+            markdownContent += `Arquivo: ${pathAfterForceApp}\n\n`;
+        } else {
+            // Caso 'force-app' não seja encontrado, usar o caminho completo
+            markdownContent += `### Análise Estática de Código - ${engine}\n`;
+            markdownContent += `Arquivo: ${fileName}\n\n`;
+        }
+
         const violations = entry.violations;
 
-        // Sumário das violações
-        markdownContent += "### Análise Estática de Código\n";
-        markdownContent += `Encontramos ${violations.length} violações de boas práticas e desempenho no seu código.\n`;
+        // Verificar se há violações
+        if (violations.length > 0) {
+            // Detalhes das violações
+            markdownContent += "#### Detalhes das Violações:\n";
+            violations.forEach((violation, index) => {
+                markdownContent += `${index + 1}. **Regra:** ${violation.ruleName}\n`;
+                markdownContent += `   - **Severidade:** ${violation.severity}\n`;
+                markdownContent += `   - **Categoria:** ${violation.category}\n`;
+                markdownContent += `   - **Descrição:** ${violation.message.trim()}\n`;
+                markdownContent += `   - **Localização:** Linha ${violation.line}, Coluna ${violation.column} até Linha ${violation.endLine}, Coluna ${violation.endColumn}\n`;
+                markdownContent += `   - **[Link para Documentação](${violation.url})**\n\n`;
+            });
+        } else {
+            markdownContent += "Não foram encontradas violações neste arquivo.\n\n";
+        }
 
-        // Detalhes das violações
-        markdownContent += "#### Detalhes das Violações:\n";
-        violations.forEach((violation, index) => {
-            markdownContent += `${index + 1}. **Regra:** ${violation.ruleName}\n`;
-            markdownContent += `   - **Severidade:** ${violation.severity}\n`;
-            markdownContent += `   - **Categoria:** ${violation.category}\n`;
-            markdownContent += `   - **Descrição:** ${violation.message.trim()}\n`;
-            markdownContent += `   - **Localização:** Linha ${violation.line}, Coluna ${violation.column} até Linha ${violation.endLine}, Coluna ${violation.endColumn}\n`;
-            markdownContent += `   - **[Link para Documentação](${violation.url})**\n\n`;
-        });
+        // Adicionar separador entre diferentes entradas, se houver mais de uma
+        markdownContent += "---\n\n";
     });
 
     // Escrever o conteúdo Markdown em um arquivo .md
     fs.writeFileSync('comment.md', markdownContent);
-    console.log('Arquivo report.md gerado com sucesso.');
+    console.log('Arquivo comment.md gerado com sucesso.');
 
 } else {
     console.error(`Arquivo não encontrado: ${filePath}`);
